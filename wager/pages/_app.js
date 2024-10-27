647af6dc7e5f3b1736e604a1f25ab5a1c52ebfee
ParkerMillerBusiness <lordbobsemail@gmail.com>
2024-10-27 16:19:20 -0500

    deposits work now

--- wager/pages/_app.js
+++
@@ -1,7 +0,0 @@
-// pages/_app.js
-import '../styles/globals.css'; // Import global CSS here only
-
-
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
